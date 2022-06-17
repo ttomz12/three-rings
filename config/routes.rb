f8a1a807e8ca4219227007774860f75ec5edfff7
Taro Tomiya <taro.tomiya@gmail.com>
2022-06-17 18:48:44 -0400

    home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'form', to: 'pages#form'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
